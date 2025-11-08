0e5f4782cc5724c685160afe36a0fb8a0fbcc3cb
PGocheva <p.gocheva@student.fontys.nl>
2025-11-08 14:16:48 +0100

    Application

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Loading the saved Random Forest model
+model = joblib.load("cad_rf_pipeline.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction_text = None
+    probability = None
+
+    if request.method == "POST":
+        # Collecting from data
+        trestbps = float(request.form["trestbps"])
+        oldpeak = float(request.form["oldpeak"])
+        sex = int(request.form["sex"])
+        fbs = int(request.form["fbs"])
+        slope = int(request.form["slope"])
+        ca = int(request.form["ca"])
+        restecg = int(request.form["restecg"])
+
+        # creating dataframe with same columns as during training
+        input_df = pd.DataFrame([{
+            "trestbps": trestbps,
+            "oldpeak": oldpeak,
+            "sex": sex,
+            "fbs": fbs,
+            "slope": slope,
+            "ca": ca,
+            "restecg": restecg
+        }])
+
+        # predicting
+        y_pred = model.predict(input_df)[0]
+        y_proba = model.predict_proba(input_df)[0][1]
+
+        if y_pred == 1:
+            prediction_text = "Model prediction: Coronary Artery Disease (class 1)"
+        else:
+            prediction_text = "Model prediction: No Coronary Artery Disease (class 0)"
+
+        probability = round(y_proba * 100, 2)
+
+    return render_template("index.html",
+                           prediction=prediction_text,
+                           probability=probability)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
